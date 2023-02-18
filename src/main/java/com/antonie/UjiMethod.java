6049179cf071067f4a07d34ec8a851b819e58a9d
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2023-02-18 08:43:03 +0700

    upload UjiMethod untuk dikerjakan

---
+++ src/main/java/com/antonie/UjiMethod.java
@@ -0,0 +1,23 @@
+package com.antonie;
+public class UjiMethod {
+    //buat method yang menerima input boolean, 
+    //jika input adalah true maka kembalikan false, selain itu true
+    public boolean ujiBoolean(boolean input){
+        //isi disini
+    }
+    //buat method yang menerima integer input, kembalikan hasil input + 15
+    public int ujiInteger(int input){
+        //isi disini
+    }
+    //buat method yang menerima double input, jika input > 2.5 maka kembalikan input + 2.5, 
+    //jika tidak kembalikan 0.5
+    public double ujiDouble(double input){
+        //isi disini
+    }
+    //buat method yang menerima string input, jika = "dutawacana" (incase sensitive) maka 
+    //return "benar", selain itu "salah"
+    public String ujiString(String input){
+        //isi disini
+    }
+
+}
